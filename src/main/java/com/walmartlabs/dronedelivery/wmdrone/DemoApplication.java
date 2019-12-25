cf585a37cbb622c7293725e095b5919e7e59e543
Anand P <priyadarshee+git@gmail.com>
2019-12-25 15:55:31 -0500

    added a shell method for poc

--- src/main/java/com/walmartlabs/dronedelivery/wmdrone/DemoApplication.java
+++ src/main/java/com/walmartlabs/dronedelivery/wmdrone/DemoApplication.java
@@ -2,12 +2,21 @@ package com.walmartlabs.dronedelivery.wmdrone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 @SpringBootApplication
+@ShellComponent
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
+	
